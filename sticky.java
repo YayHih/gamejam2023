1c5489c5cb584e41390c9d626bf7688de1a05c92
YayHih <78941861+YayHih@users.noreply.github.com>
2023-02-18 11:27:02 -0500

    commit

--- sticky.java
+++ sticky.java
@@ -64,6 +64,7 @@ public class sticky extends JFrame {
     // animation selection
     int aSelection = 4;
     boolean physics = true;
+    boolean movements = false;
     int timeSpent = 0;
     int finalTime = 0;
     Shape player = new Rectangle2D.Double(x, (int) y, 50, 50);
@@ -108,18 +109,12 @@ public class sticky extends JFrame {
         theCanvas.setFocusable(true);
         add(theCanvas);
         imageSplicer();
-        while (!player.intersects(5, 900, 1900, 20)) {
-            /*----------------------------------------------------------------------------------------------------------------
-            for people with weaker systems, you can have it check for inputs every 10 ms+,
-            for an easier time on the cpu/gpu  2ms 40cpu  50 gpu and without the 2ms its runs at 45cpu 58gpu
-            // */
-            if ((System.currentTimeMillis() - timer) % 2 == 0) {
-                repaint();
-            }
-        }
+        repaint();
+        start();
     }
 
     public void imageSplicer() {
+
         int rows = 1;
         int columns = 9;
 
@@ -151,6 +146,25 @@ public class sticky extends JFrame {
         }
     }
 
+    public void start() {
+        while (!player.intersects(5, 900, 1900, 20)) {
+            /*----------------------------------------------------------------------------------------------------------------
+            for people with weaker systems, you can have it check for inputs every 10 ms+,
+            for an easier time on the cpu/gpu  2ms 40cpu  50 gpu and without the 2ms its runs at 45cpu 58gpu
+            // */
+            if ((System.currentTimeMillis() - timer) % 5 == 0 && movements) {
+                if (mgen) {
+                    mapgen();
+                }
+                movement();
+                physics();
+            }
+            if ((System.currentTimeMillis() - timer) % 15 == 0 || !movements) {
+                repaint();
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         sticky main = new sticky();
 
@@ -168,6 +182,8 @@ public class sticky extends JFrame {
             eventHistory = new MouseEvent[MAX_EVENTS];
             totalEvents = 0;
             setFocusable(true);
+            long startTimer = System.currentTimeMillis();
+            int currentTicks = 0;
 
         }
 
@@ -202,8 +218,7 @@ public class sticky extends JFrame {
 
             } else if (events >= 1 && !win) {
                 g.setFont(new Font("Arial", 10, 30));
-                movement();
-                physics();
+                movements = true;
 
                 // for background "gif" display
                 if ((int) (System.currentTimeMillis() - timer) % 250 == 0) {
@@ -215,9 +230,6 @@ public class sticky extends JFrame {
                 g.drawImage(background, 0, backgroundy, null);
                 // map generation
 
-                if (mgen) {
-                    mapgen();
-                }
                 // base platform
                 g.drawImage(basePlatform, 0, 900, null);
                 // displays the player based off of the x1 var, or how many times 'a' or 'd'
@@ -280,7 +292,6 @@ public class sticky extends JFrame {
             } else if (win) {
                 /*
                  * My attempt at adding in a file to record the highscore, although for some
-                 * reaseon, kept throughing errors, so I decided not to implement.
                  * try {
                  * File myObj = new File("highScore.txt");
                  * if (myObj.createNewFile()) {
@@ -350,84 +361,40 @@ public class sticky extends JFrame {
             }
         }
 
-        public void movement() {
-            if (input == ' ' && speed == 0 && touching) {
-                speed -= 3;
-                physics = true;
-                y -= 2;
-                y1 = 70;
-
-            }
-            if (input == 'd' && x1 < 40) {
-                for (int z = 0; z < 5; z++) {
-                    x1 += 1;
-                }
-                vSpeed -= 1;
-                aSelection -= 1;
-            } else if (input == 'a' && x1 > 0) {
-                for (int z = 0; z < 5; z++) {
-                    x1 -= 1;
-                }
-                vSpeed += 1;
-                aSelection += 1;
-            }
-            // changes the input to another char that way the input chagens and it doesnt
-            // keep running the same code
-            input = 'z';
-            // checks to see the bounds, if out of bounds, either updates the map or resets
-            // position
-            if (x > 1960) {
-                mgen = true;
-                x = 20;
-                iValue += platformNum;
-            }
-            if (x < 5 && iValue != 0) {
-                mgen = true;
-                x = 1950;
-                iValue -= platformNum;
-            }
-            if (x < 5 && (iValue / platformNum + 1) == 1) {
-                vSpeed = 0;
-                x1 = 20;
-                aSelection = 4;
-                x += 1;
-            }
-        }
-
-        public void mapgen() {
-            hA[1] = 500;
-            for (int z = 0; z <= platformNum; z++) {
-                int x1 = (int) Math.round(Math.random() * 2000);
-                // while you can just make the range up to 800, its better like this beacuse it
-                // means that there will be a higher probablity of platforms on the lower level.
-                int y1 = (int) Math.round(Math.random() * 1000);
-                int sx1 = (int) Math.round(Math.random() * 70);
-                // makes sure that the platforms are in reasonable locations
-                if (x1 < 150) {
-                    x1 += 150;
-                } else if (x1 > 1800) {
-                    x1 -= 200;
-                }
-                if (y1 < 60) {
-                    y1 += 60;
-                }
-
-                else if (y1 > 800) {
-                    y1 -= 150;
-                }
-                x1a.add(x1);
-                y1a.add(y1);
-                sx1a.add(sx1);
-
-            }
-            timer = System.currentTimeMillis();
-            mgen = false;
-        }
-
         public void addEvent(MouseEvent e) {
             eventHistory[totalEvents++] = e;
         }
 
+    }
+
+    public void mapgen() {
+        hA[1] = 500;
+        for (int z = 0; z <= platformNum; z++) {
+            int x1 = (int) Math.round(Math.random() * 2000);
+            // while you can just make the range up to 800, its better like this beacuse it
+            // means that there will be a higher probablity of platforms on the lower level.
+            int y1 = (int) Math.round(Math.random() * 1000);
+            int sx1 = (int) Math.round(Math.random() * 70);
+            // makes sure that the platforms are in reasonable locations
+            if (x1 < 150) {
+                x1 += 150;
+            } else if (x1 > 1800) {
+                x1 -= 200;
+            }
+            if (y1 < 60) {
+                y1 += 60;
+            }
+
+            else if (y1 > 800) {
+                y1 -= 150;
+            }
+            x1a.add(x1);
+            y1a.add(y1);
+            sx1a.add(sx1);
+
+        }
+        timer = System.currentTimeMillis();
+        mgen = false;
     }
 
     public boolean touching() {
@@ -473,52 +440,94 @@ public class sticky extends JFrame {
         return physics;
     }
 
+    public void movement() {
+        if (input == ' ' && speed == 0 && touching) {
+            speed -= 3;
+            physics = true;
+            y -= 2;
+            y1 = 70;
+
+        }
+        if (input == 'd' && x1 < 40) {
+            for (int z = 0; z < 5; z++) {
+                x1 += 1;
+            }
+            vSpeed -= 1;
+            aSelection -= 1;
+        } else if (input == 'a' && x1 > 0) {
+            for (int z = 0; z < 5; z++) {
+                x1 -= 1;
+            }
+            vSpeed += 1;
+            aSelection += 1;
+        }
+        // changes the input to another char that way the input chagens and it doesnt
+        // keep running the same code
+        input = 'z';
+        // checks to see the bounds, if out of bounds, either updates the map or resets
+        // position
+        if (x > 1960) {
+            mgen = true;
+            x = 20;
+            iValue += platformNum;
+        }
+        if (x < 5 && iValue != 0) {
+            mgen = true;
+            x = 1950;
+            iValue -= platformNum;
+        }
+        if (x < 5 && (iValue / platformNum + 1) == 1) {
+            vSpeed = 0;
+            x1 = 20;
+            aSelection = 4;
+            x += 1;
+        }
+    }
+
     public void physics() {
-        // updates every 10ms for optimization
-        if ((System.currentTimeMillis() - timer) % 10 == 0) {
-            if (speed > 0) {
-                for (int z = 0; z < speed; z++) {
-                    // if player is off the ground, gravity works
-                    if (!touching()) {
-                        y += 1;
-                    } else if (touching()) {
-                        // if player is on the ground, gravity doesnt work
-                        speed = 0;
+
+        if (speed > 0) {
+            for (int z = 0; z < speed; z++) {
+                // if player is off the ground, gravity works
+                if (!touching()) {
+                    y += 1;
+                } else if (touching()) {
+                    // if player is on the ground, gravity doesnt work
+                    speed = 0;
+                }
+                // vertical transform
+                if (vSpeed > 0 && touching == false) {
+                    for (i = 0; i < vSpeed; i++) {
+                        x += 1;
                     }
-                    // vertical transform
-                    if (vSpeed > 0 && touching == false) {
-                        for (i = 0; i < vSpeed; i++) {
-                            x += 1;
-                        }
-                    } else if (vSpeed < 0 && touching == false) {
-                        for (i = 0; i < -vSpeed; i++) {
-                            x -= 1;
-                        }
+                } else if (vSpeed < 0 && touching == false) {
+                    for (i = 0; i < -vSpeed; i++) {
+                        x -= 1;
                     }
                 }
-            } else {
-                // brings the player upwards
-                for (z = 0; z < -speed; z++) {
-                    if (y > -1000) {
-                        y -= 1;
-                        // speed = 0;
-                    } else {
-                        speed = 1;
+            }
+        } else {
+            // brings the player upwards
+            for (z = 0; z < -speed; z++) {
+                if (y > -1000) {
+                    y -= 1;
+                    // speed = 0;
+                } else {
+                    speed = 1;
+                }
+                // vertical transform
+                if (vSpeed > 0) {
+                    for (i = 0; i < vSpeed; i++) {
+                        x += 1;
                     }
-                    // vertical transform
-                    if (vSpeed > 0) {
-                        for (i = 0; i < vSpeed; i++) {
-                            x += 1;
-                        }
-                    } else if (vSpeed < 0) {
-                        for (i = 0; i < -vSpeed; i++) {
-                            x -= 1;
-                        }
+                } else if (vSpeed < 0) {
+                    for (i = 0; i < -vSpeed; i++) {
+                        x -= 1;
                     }
-
                 }
 
             }
+
         }
 
         if ((int) (System.currentTimeMillis() - timer) % 40 == 0 && !touching()) {
